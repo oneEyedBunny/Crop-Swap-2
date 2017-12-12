0892b36fba739596c8781c6365ed7a5f05457248
Sandra <sandraultreras@gmail.com>
2017-12-11 18:13:22 -0800

    Got map to get addresses from neighborhood DB

--- public/scripts/googleMap.js
+++ public/scripts/googleMap.js
@@ -32,57 +32,25 @@ $(document).ready(function() {
 
 function makeRequest() {
   $.get('/neighborhoods', function(data) {
-    const mapData = data.map(function(neighborhood) {
-      return getCoord(neighborhood.address);
+      data.forEach(function(neighborhood) {
+      addMarker(neighborhood);
     });
-    console.log(mapData);
-    // $('#map').text(data);
   })
 }
 
-function getCoord(address) {
+function addMarker(neighborhood) {
   var geocoder = new google.maps.Geocoder();
 
-  geocoder.geocode( { 'address': address}, function(results, status) {
+  geocoder.geocode( { 'address': neighborhood.address}, function(results, status) {
 
     if (status == google.maps.GeocoderStatus.OK) {
       var latitude = results[0].geometry.location.lat();
       var longitude = results[0].geometry.location.lng();
-      alert(latitude);
-      return {lat: latitude, lng: longitude};
-
+      new google.maps.Marker({
+        position: {lat: latitude, lng: longitude},
+        map: map,
+        title: neighborhood.neighborhood_name
+      });
     }
   });
-
 }
-
-
-//-----------
-// var map;
-//       function initMap() {
-//         map = new google.maps.Map(document.getElementById('map'), {
-//           zoom: 2,
-//           center: new google.maps.LatLng(2.8,-187.3),
-//           mapTypeId: 'terrain'
-//         });
-//
-//         // Create a <script> tag and set the USGS URL as the source.
-//         var script = document.createElement('script');
-//         // This example uses a local copy of the GeoJSON stored at
-//         // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-//         script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-//         document.getElementsByTagName('head')[0].appendChild(script);
-//       }
-//
-//       // Loop through the results array and place a marker for each
-//       // set of coordinates.
-//       window.eqfeed_callback = function(results) {
-//         for (var i = 0; i < results.features.length; i++) {
-//           var coords = results.features[i].geometry.coordinates;
-//           var latLng = new google.maps.LatLng(coords[1],coords[0]);
-//           var marker = new google.maps.Marker({
-//             position: latLng,
-//             map: map
-//           });
-//         }
-//       }
