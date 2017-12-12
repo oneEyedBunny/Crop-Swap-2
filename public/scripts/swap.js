4e838b62ada97ede374c1735dcda78440726227c
Sandra <sandraultreras@gmail.com>
2017-12-11 21:05:31 -0800

    working on crop drop down linked with neighborhood

--- public/scripts/swap.js
+++ public/scripts/swap.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   $.get('/crops').then(function(cropData) {
     console.log(cropData);
-  })
+    cropData.forEach(function (ele) {
+      var $option = $('<option>').attr('value', ele.crop_name).text(ele.crop_name);
+        $('#produce-location-options').append($option)
+      })
+    });
 })
